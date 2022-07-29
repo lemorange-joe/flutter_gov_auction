9dcca9ef5c6c59bd31162859ee6d8e7261530132
legco-jkytse <jkytse@legco.gov.hk>
2022-07-29 17:40:03 +0800

    setup api

--- php/api/index.php
+++ php/api/index.php
@@ -1,5 +1,50 @@
 <?php
-echo date('Y-m-d H:i:s');
+include_once ('../include/config.php');
+
+if ($env == 'dev') {
+  ini_set('display_errors', 'on');
+  error_reporting(E_ALL);
+} else {
+  ini_set('display_errors', '0');
+  error_reporting(0);
+  // error_reporting(E_ALL | E_STRICT);  # ...but do log them
+}
+
+$request = explode('-', $_REQUEST['req']);
+$controller = preg_replace('/[^a-z0-9_]+/i', '', array_shift($request));
+$method = preg_replace('/[^a-z0-9_]+/i', '', array_shift($request));
+$param = array_shift($request);
+$lang = "en";
+var_dump($request);
+if (!empty($request)) {
+  $temp_lang = array_shift($request);
+  if (strtolower($temp_lang) == "sc") {
+    $lang = "sc";
+  } else if (strtolower($temp_lang) == "ch") {
+    $lang = "ch";
+  }
+}
+
+echo 'controller: ' . $controller;
+echo '<br>method: ' . $method;
+echo '<br>lang: ' . $lang;
 echo '<hr />';
-echo $_REQUEST['req'];
+// ========================================================================
+
+class DataController {
+  function getAppInfo($keyword) {
+    echo 'getAppInfo: ' . date('Y/m/d H:i:s');
+    echo '<br>keyword: ' . $keyword;
+  }
+}
+
+// ========================================================================
+
+if ($controller == "data") {
+  $data = new DataController();
+
+  if (method_exists($data, $method)) {
+    $data->$method($param);
+  }
+}
 ?>
