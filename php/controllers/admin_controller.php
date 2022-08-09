a9cc3d80169b2dfc99fd0c4b6a0fde73f7ff5c82
legco-jkytse <jkytse@legco.gov.hk>
2022-08-09 16:05:52 +0800

    admin processAuctionListText extract data

--- php/controllers/admin_controller.php
+++ php/controllers/admin_controller.php
@@ -1,11 +1,16 @@
 <?php
 include_once ('../include/demo_data.php');
+include_once ('../include/enum.php');
 
 class AdminController {
+  public $regSkipLine = '/(拍賣物品清單編號*)|(AUCTION LIST *)|(- \d* -$)|(Lot No. Item No. Description Quantity)|(批號 項目 物品詳情 數量)/';
+
   function parseData() {
     $txt = $GLOBALS['DEMO_AUCTION_PDF'];
     $strAuctionList = $this->splitAuctionListText($txt);
-    Debug_var_dump($strAuctionList);
+    foreach($strAuctionList as $strAuction) {
+      $this->processAuctionListText($strAuction, ItemType::UnclaimedProperties);
+    }
   }
 
   function splitAuctionListText($txt) {
@@ -58,7 +63,9 @@ class AdminController {
       }
       
       if ($foundBeginning) {
-        $curLines[] = $line;
+        if (!preg_match($this->regSkipLine, $line)) {
+          $curLines[] = $line;
+        }
         ++$i;
       }
       
@@ -66,6 +73,49 @@ class AdminController {
     $output[] = implode("\n", $curLines);
 
     return $output;
+  }
+
+  function processAuctionListText($strAuction, $itemType) {
+    $matchValues = array();
+    $patterns = array(
+      "lotNum" => '/\n' . $itemType . '-((\d)+) 1\./i',
+      "gldFileRef" => '/\(GLD File Ref.*:\s*(.*)\)/i',
+      "reference" => '/Reference\s*:\s*(.*)/i',
+      "departmentEn" => '/Department\s*:\s*(.*)/i',
+      "departmentTc" => '/部門\s*:\s*(.*)/i',
+      "contactPersonEn" => '/Contact Person\s*:\s*(.*)\s*on\s*/i',
+      "contactPersonTc" => '/聯絡人\s*:\s*(.*)\s*電話\s*:/i',
+      "contactNumberEn" => '/Contact Person\s*:\s*.*\s*on\s*(.*)/i',
+      "contactNumberTc" => '/聯絡人\s*:.*電話\s*:\s*(.*)/i',
+      "locationEn" => '/Location\s*:\s*((.|\n)*)Contact Person\s*:/i',
+      "locationTc" => '/地點\s*:\s*((.|\n)*)聯絡人\s*:/i',
+      "remarksEn" => '/Remarks.*:((.|\n)*)注意事項/i',
+      "remarksTc" => '/注意事項.*:((.|\n)*)。\n[a-zA-Z]+/i',
+    );
+
+    // loop through the patterns and assign the matched values into $matchValues
+    foreach(array_keys($patterns) as $key) {
+      preg_match($patterns[$key], $strAuction, $matches);
+
+      if (count($matches) > 1) {
+        $matchValues[$key] = $matches[1];
+      } else {
+        $matchValues[$key] = "";
+      }
+    }
+
+    // post process add back characters
+    if ($matchValues["remarksTc"] != "") {
+      $matchValues["remarksTc"] .= "。";
+    }
+    if ($matchValues["lotNum"] != "") {
+      $matchValues["lotNum"] = $itemType . "-" . $matchValues["lotNum"];
+    }
+
+    foreach(array_keys($matchValues) as $key) {
+      echo "$key: " . $matchValues[$key] . "<br>";
+    }
+    echo "<textarea style='width: 800px; height: 300px'>$strAuction</textarea> <br />";
   }
 }
 ?>
--- php/controllers/data_controller.php
+++ php/controllers/data_controller.php
@@ -3,6 +3,7 @@ class DataController {
   function getAppInfo($param) {
     echo 'getAppInfo: ' . date('Y/m/d H:i:s');
     Debug_var_dump($param);
+    echo $_SERVER['SERVER_NAME'] . "@" . date('Y/m/d H:i:s');
   }
 }
 ?>
